779e77381a9eabcdb63912c74e6cd1d2513905ac
yelluw <pr@yelluw.com>
2017-07-29 18:07:39 -0400

    updated ContactForm form fields

--- cms/forms.py
+++ cms/forms.py
@@ -4,7 +4,16 @@ from django import forms
 class ContactForm(forms.Form):
     """
     Contact form for general inquiries
+
+    TODO:
+        - See if using # https://github.com/stefanfoulis/django-phonenumber-field
+          is a good choice for this instance.
+
     """
+    inquiry_type = forms.ChoiceField()
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': ''}), max_length=140)
+    email = forms.EmailFiel(widget=forms.EmailInput(attrs={'class': ''}))
+    company = forms.CharField(widget=forms.TextInput(attrs={'class': ''}), max_length=140)
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': ''}), max_length=40)
     message = forms.CharField(widget=forms.Textarea(attrs={}))
-    subject = forms.CharField(widget=forms.TextInput(attrs={'class': ''}), max_length=140)
-    sender = forms.EmailFiel(widget=forms.EmailInput(attrs={'class': ''}))
+
