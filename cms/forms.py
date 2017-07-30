97bec5402c7db33b53af677c4d9cbf6cfbc1225d
yelluw <pr@yelluw.com>
2017-07-30 03:38:20 -0400

    cleanup whitespace

--- cms/forms.py
+++ cms/forms.py
@@ -15,11 +15,9 @@ class WorkInquiryContactForm(forms.Form):
 
     ATTRS = {"class": "form-control"}
 
-
     inquiry_type = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
     name = forms.CharField(widget=forms.TextInput(attrs=ATTRS), max_length=140)
     email = forms.EmailField(widget=forms.EmailInput(attrs=ATTRS))
     company = forms.CharField(widget=forms.TextInput(attrs=ATTRS), max_length=140)
     phone = forms.CharField(widget=forms.TextInput(attrs=ATTRS), max_length=40)
     message = forms.CharField(widget=forms.Textarea(attrs=ATTRS))
-
