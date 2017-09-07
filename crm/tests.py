a95fc4ae81a202ca529f1837c35fe660c234c453
yelluw <pr@yelluw.com>
2017-09-07 02:29:44 -0400

    crm and drip unit and form tests

--- crm/tests.py
+++ crm/tests.py
@@ -1,18 +1,91 @@
+from datetime import datetime
+
 from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse_lazy
 
-from .forms import WorkInquiryContactForm
+from crm.models import WorkInquiryContact
+from crm.forms import WorkInquiryContactForm
 
 #command: python3 manage.py test --verbosity=2
+
+
+class WorkInquiryContactUnitTest(TestCase):
+    """
+    WorkInquiryContact model unit tests
+    """
+    pass
+
+    def setUp(self):
+        self.work_inquiry_contact = WorkInquiryContact.objects.create(
+            inquiry_type="general",
+            name="mr test",
+            email="a@a.com",
+            company="test corp",
+            phone="1234567890",
+            message="hello test"
+        )
+
+
+    def test_inquiry_type_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.inquiry_type, str)
+
+
+    def test_inquiry_type_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.inquiry_type, "general")
+
+
+    def test_name_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.name, str)
+
+
+    def test_name_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.name, "mr test")
+
+
+    def test_email_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.email, str)
+
+
+    def test_email_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.email, "a@a.com")
+
+
+    def test_company_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.company, str)
+
+
+    def test_company_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.company, "test corp")
+
+
+    def test_phone_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.phone, str)
+
+
+    def test_phone_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.phone, "1234567890")
+
+
+    def test_message_field_type(self):
+        self.assertIsInstance(self.work_inquiry_contact.message, str)
+
+
+    def test_message_field_value(self):
+        self.assertEqual(self.work_inquiry_contact.message, "hello test")
+
+
+    def test_received_on_field(self):
+        self.assertIsInstance(self.work_inquiry_contact.received_on, datetime)
+
 
 class CrmIntegrationTest(TestCase):
     """
     Integration tests for views
+
+    TODO
     """
     pass
-
-
 
 
 class WorkInquiryFormUnitTest(TestCase):
--- drip/tests.py
+++ drip/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse_lazy
+
+from drip.models import DripSubscriber
+from drip.forms import DripSubscriberForm
+
+#command: python3 manage.py test --verbosity=2
+
+
+class DripSubscriberUnitTest(TestCase):
+    """
+    DripsSubscriber model unit test
+    """
+
+
+    def setUp(self):
+        self.drip_subscriber = DripSubscriber.objects.create(
+            email="a@a.com",
+            funnel_entry_point="URI: test, location: test"
+        )
+
+
+    def test_email_field_type(self):
+        self.assertIsInstance(self.drip_subscriber.email, str)
+
+
+    def test_email_field_value(self):
+        self.assertEqual(self.drip_subscriber.email, "a@a.com")
+
+
+    def test_funnel_entry_point_field_type(self):
+        self.assertIsInstance(self.drip_subscriber.funnel_entry_point, str)
+
+
+    def test_funnel_entry_point_field_value(self):
+        self.assertEqual(self.drip_subscriber.funnel_entry_point, "URI: test, location: test")
+
+
+    def test_active_field_type(self):
+        self.assertIsInstance(self.drip_subscriber.active, bool)
+
+
+    def test_active_field_value(self):
+        self.assertEqual(self.drip_subscriber.active, True)
+
+
+    def test_created_field(self):
+        self.assertIsInstance(self.drip_subscriber.created, datetime)
+
+
+class DripIntegrationTest(TestCase):
+    """
+    Integrations tests for drip.views
+    TODO
+    """
+    pass
+
+
+class DripSubscriberFormUnittest(TestCase):
+    """
+    Test the actual DripSubscriberForm
+    and not the view
+    """
+
+
+    def test_drip_subscriber_form(self):
+        data = {
+            "email":"a@a.com",
+            "funnel_entry_point":"URI: test, location: test"
+        }
+        form = DripSubscriberForm(data=data)
+        self.assertTrue(form.is_valid())
