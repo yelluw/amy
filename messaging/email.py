a3727d114c8c5159b3da624022340debff2835ea
Yelluw <pr@yelluw.com>
2017-09-10 16:36:27 -0400

    initial commit

---
+++ messaging/email.py
@@ -0,0 +1,22 @@
+"""
+Utilities to send
+email.
+"""
+
+
+def send_drip_email(to, subject, message):
+    """
+    Sends a drip email to a single recipient
+    using django's send_mail() functionality
+    """
+    pass
+
+
+
+def send_drip_emails(to_list, subject, message):
+    """
+    Sends a drip email to a list of
+    recipients using django's
+    send_mass_mail() functionality
+    """
+    pass
