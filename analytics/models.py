b7f0b00e8ee501dcc0b11ce9cae0aff607055353
Yelluw <pr@yelluw.com>
2017-09-14 23:54:04 -0400

    initial commit of analytics

---
+++ analytics/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Visitor(models.Model):
+    """
+    Defines a visitor to amy
+    """
+    pass
+
+    # ip addres
+    # user agent
+    # host header
+    # timestamp (local)
