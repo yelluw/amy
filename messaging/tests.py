b7f44aa1e894213d5255a489f3132708c22a7354
Yelluw <pr@yelluw.com>
2017-09-16 07:09:57 -0400

    Update send_email and send_multiple_emails function parameters and its unit tests

--- messaging/tests.py
+++ messaging/tests.py
@@ -12,14 +12,14 @@ class SendEmailUnitTest(TestCase):
     def setUp(self):
         self.subject = "test subject"
         self.message = "test message"
-        self.to = "pryelluw@gmail.com"
+        self.to_address = "pryelluw@gmail.com"
 
 
     def test_send_email_returns_one_on_success(self):
-        sent = send_email(self.to, self.subject, self.message)
+        sent = send_email(self.subject, self.message, self.to_address)
         self.assertTrue(sent)
 
 
     def test_send_email_returns_zero_on_failure(self):
-        sent = send_email("", self.subject, self.message)
+        sent = send_email(self.subject, self.message, "")
         self.assertFalse(sent)
